3197219895a7b2eccd51a20b2a6ba9f476ccda53
robert <robertesclapez@gmail.com>
2022-08-25 19:21:41 +0200

    Add common.h header to speedup things

---
+++ common.h
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <limits>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <string>
+
+using namespace std;
