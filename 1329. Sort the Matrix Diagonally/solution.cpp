c3768438a540c639fd4de1c1382908f893d665e4
robert <robertesclapez@gmail.com>
2022-08-28 22:27:12 +0200

    Add solution to 1329

--- 1329. Sort the Matrix Diagonally/solution.cpp
+++ 1329. Sort the Matrix Diagonally/solution.cpp
@@ -1,25 +1,24 @@
 #include "../common.h"
-#include <queue>
 
 class Solution {
 public:
-    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-      unsigned cols = mat.size();        
-      unsigned rows = mat.front().size();
+  vector<vector<int>> diagonalSort(vector<vector<int>> &mat) {
+    unsigned cols = mat.size();
+    unsigned rows = mat.front().size();
 
-      unordered_map<int, priority_queue<int, vector<int>, greater<int>>> map;
-      for (int i = 0; i < rows;  i++) 
-        for (int j = 0; j < cols;  j++) {
-          map[i-j].push(mat[i][j]);
-        } 
-
-      for (unsigned i = 0; i < rows;  i++) 
-        for (unsigned j = 0; j < cols;  j++) {
-          auto &pq = map[i-j];
-          mat[i][j] = pq.top(); pq.pop();
-
+    unordered_map<int, priority_queue<int, vector<int>, greater<int>>> map;
+    for (int i = 0; i < rows; i++)
+      for (int j = 0; j < cols; j++) {
+        map[i - j].push(mat[i][j]);
       }
 
-      return mat;
-    }
+    for (unsigned i = 0; i < rows; i++)
+      for (unsigned j = 0; j < cols; j++) {
+        auto &pq = map[i - j];
+        mat[i][j] = pq.top();
+        pq.pop();
+      }
+
+    return mat;
+  }
 };
